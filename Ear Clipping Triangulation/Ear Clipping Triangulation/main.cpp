6fee20aa92582df8d9333e97314b152405d65b05
Wawrzyn321 <Wawrzyn321@gmail.com>
2016-09-25 00:06:44 +0200

    Removed main.h (why was it here?), further refactor

--- Ear Clipping Triangulation/Ear Clipping Triangulation/main.cpp
+++ Ear Clipping Triangulation/Ear Clipping Triangulation/main.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <windows.h>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <windows.h>
 #include "utility.h"
-#include "Shapes.h"
 #include <vector>
-#include "main.h"
+#include "Shapes.h"
 
 using namespace std;
 
 sf::RenderWindow window(sf::VideoMode(800, 600), "triangulation");
-
-
 
 vector<point> points;
 vector<line> lines;
@@ -31,26 +26,30 @@ void refreshWindow(){
 	window.display();
 }
 
-
 inline bool isPointConvex(point p1, point p2, point p3, bool isCW) {
     return isPointConvex(p1.getPosition(), p2.getPosition(), p2.getPosition(), p3.getPosition(), isCW);
 }
 
-bool isClockwise(){//usable only in this project
+bool isShapeClockWise(vector<point> &sourcePoints){
 	double sum = 0;
-	for (unsigned int i = 0; i < points.size(); i++){
+	for (unsigned int i = 0; i < sourcePoints.size(); i++){
+
 		unsigned int k1 = i + 1;
+        if (k1 >= sourcePoints.size())
+            k1 -= sourcePoints.size();
+
         unsigned int k2 = i + 2;
-		if (k1 >= points.size()) k1 %= points.size();
-		if (k2 >= points.size()) k2 %= points.size();
-		if (areVecsConvex(points[i].getPosition(), points[k1].getPosition(), points[k2].getPosition())){
-			sum += angleBetweenPoints(points[i].getPosition(), points[k1].getPosition(), points[k2].getPosition());
+		if (k2 >= sourcePoints.size())
+            k2 -= sourcePoints.size();
+
+		if (areVecsConvex(sourcePoints[i].getPosition(), sourcePoints[k1].getPosition(), sourcePoints[k2].getPosition())){
+			sum += angleBetweenPoints(sourcePoints[i].getPosition(), sourcePoints[k1].getPosition(), sourcePoints[k2].getPosition());
 		}
 		else{
-			sum += 360 - angleBetweenPoints(points[i].getPosition(), points[k1].getPosition(), points[k2].getPosition());
+			sum += 360 - angleBetweenPoints(sourcePoints[i].getPosition(), sourcePoints[k1].getPosition(), sourcePoints[k2].getPosition());
 		}
 	}
-    return round(sum) == 180 * (points.size() - 2);
+    return round(sum) == 180 * (sourcePoints.size() - 2);
 }
 
 void addPoint(float  nX, float  nY) {
@@ -63,16 +62,19 @@ void addPoint(float  nX, float  nY) {
     points.push_back(point(nX, nY));
 
     //draw line, if we have more than 1 point
-    if (points.size()>1)
-        lines.push_back(line(points[points.size()-2], points[points.size()-1]));
+    if (points.size() > 1) {
+        lines.push_back(line(points[points.size() - 2], points[points.size() - 1]));
+    }
 }
 
-void closeShape(bool delayEnabled){
+//with effects
+void triangulation(const vector<point> &sourcePoints, bool delayEnabled, bool isCool = false){
     int delay;
+    //calculate nice delay
     if (delayEnabled) {
         delay = 750;
-        if (points.size() > 15) {
-            delay = delay - points.size() * 25;
+        if (sourcePoints.size() > 15) {
+            delay = delay - sourcePoints.size() * 25;
             if (delay < 10)
                 delay = 10;
             if (delay >= 500)
@@ -81,66 +83,85 @@ void closeShape(bool delayEnabled){
     }
     else {
         delay = 0;
+        isCool = false;
     }
-	lines.push_back(line(points[0], points[points.size() - 1]));
-	refreshWindow();
-	vector<point> verts = points;
-	bool cw = isClockwise();
+
+    if (delayEnabled) {
+        refreshWindow();
+        Sleep(delay);
+    }
+
+    //temporary vector of points
+	vector<point> verts = sourcePoints;
+
+    //was shape drew clockwise?
+	bool isCW = isShapeClockWise(verts);
+    unsigned int start = 0;
 	while (verts.size() > 2){
 		bool earNotFound = true;
-        int s = 0;
-        unsigned int t = 0;
-		while (earNotFound){
-            t++;
-			s %= verts.size();
-			int m = (s + 1) % verts.size();
-			int e = (s + 2) % verts.size();
-			float xS = verts[s].x;
-			float yS = verts[s].y;
-			float xM = verts[m].x;
-			float yM = verts[m].y;
-			float xE = verts[e].x;
-			float yE = verts[e].y;
 
-			triangles.push_back(triangle(verts[s], verts[m], verts[e]));
-			bool isConvex = false;
-			if (isPointConvex(verts[s].getPosition(), verts[m].getPosition(), verts[m].getPosition(), verts[e].getPosition(), !cw)
-                && isPointConvex(verts[m].getPosition(), verts[e].getPosition(), verts[e].getPosition(), verts[s].getPosition(), !cw)
-                && isPointConvex(verts[e].getPosition(), verts[s].getPosition(), verts[s].getPosition(), verts[m].getPosition(), !cw))
-				isConvex = true;
-			if (isConvex){
-				triangles[triangles.size() - 1].tri.setFillColor(sf::Color::Green);
-			}
-			else{
+        //when triangulation isCool and delay is enabled
+        if (isCool) {
+            //only visual, while isCool==true performance is a little bit worse
+            start = 0;
+        }
+        else {
+            ;
+        }
+
+		while (earNotFound){
+			start %= verts.size();
+            unsigned int middle = (start + 1) % verts.size();
+            unsigned int end = (start + 2) % verts.size();
+
+            triangles.push_back(triangle(verts[start], verts[middle], verts[end]));
+            //is current point convex?
+            bool isConvex =
+                isPointConvex(verts[start].getPosition(), verts[middle].getPosition(), verts[middle].getPosition(), verts[end].getPosition(), !isCW)
+                && isPointConvex(verts[middle].getPosition(), verts[end].getPosition(), verts[end].getPosition(), verts[start].getPosition(), !isCW)
+                && isPointConvex(verts[end].getPosition(), verts[start].getPosition(), verts[start].getPosition(), verts[middle].getPosition(), !isCW);
+
+			if (!isConvex){
+                //reject the triangle
 				triangles[triangles.size() - 1].tri.setFillColor(sf::Color::Red);
-				s++;
+				start++;
+                triangles.erase(triangles.begin() + triangles.size() - 1);
+                continue;
 			}
-			bool noPointsIn = true;
-			if (isConvex){
-				for (unsigned int i = 0; i < verts.size(); i++)
-					if (i != s && i != m && i != e)
-						if (isPointInTriangle(verts[i].getPosition(), verts[s].getPosition(), verts[m].getPosition(), verts[e].getPosition()) && noPointsIn)
-							noPointsIn = false;
-				if (noPointsIn){
-					//cout << "no points in triangle" << endl;
-					earNotFound = false;
-				}
-				else{
-					triangles[triangles.size() - 1].tri.setFillColor(sf::Color::Red);
-					//cout << "point in triangle" << endl;
-					s++;
-				}
-			}
+
+            bool noPointsIn = true;
+
+            triangles[triangles.size() - 1].tri.setFillColor(sf::Color::Green);
+            for (unsigned int i = 0; i < verts.size(); i++)
+                if (i != start && i != middle && i != end)
+                    if (isPointInTriangle(verts[i].getPosition(), verts[start].getPosition(), verts[middle].getPosition(), verts[end].getPosition()) && noPointsIn) {
+                        noPointsIn = false;
+                        break;
+                    }
+
+            if (noPointsIn) {
+                //we found ear
+                earNotFound = false;
+            }
+            else {
+                //there's a point in triangle
+                triangles[triangles.size() - 1].tri.setFillColor(sf::Color::Red);
+                start++;
+            }
+
             if (delayEnabled) {
                 refreshWindow();
                 Sleep(delay);
             }
-			if (!noPointsIn || !isConvex){
+			if (!noPointsIn){
+                //reject the triangle
 				triangles.erase(triangles.begin() + triangles.size() - 1);
 			}
 			else{
-				verts.erase(verts.begin() + m);
+                //add triangle to set
+				verts.erase(verts.begin() + middle);
 				triangles[triangles.size() - 1].tri.setFillColor(sf::Color::White);
+
                 if (delayEnabled) {
                     refreshWindow();
                     Sleep(delay);
@@ -150,13 +171,67 @@ void closeShape(bool delayEnabled){
 	}
 }
 
+//no effects
+void triangulation(const vector<point> &sourcePoints) {
+    //temporary vector of points
+    vector<point> verts = sourcePoints;
+
+    //was shape drew clockwise?
+    bool isCW = isShapeClockWise(verts);
+    unsigned int start = 0;
+    while (verts.size() > 2) {
+
+        bool earNotFound = true;
+        while (earNotFound) {
+            start %= verts.size();
+            unsigned int middle = (start + 1) % verts.size();
+            unsigned int end = (start + 2) % verts.size();
+
+            triangles.push_back(triangle(verts[start], verts[middle], verts[end]));
+            //is current point convex?
+            bool isConvex =
+                isPointConvex(verts[start].getPosition(), verts[middle].getPosition(), verts[middle].getPosition(), verts[end].getPosition(), !isCW)
+                && isPointConvex(verts[middle].getPosition(), verts[end].getPosition(), verts[end].getPosition(), verts[start].getPosition(), !isCW)
+                && isPointConvex(verts[end].getPosition(), verts[start].getPosition(), verts[start].getPosition(), verts[middle].getPosition(), !isCW);
+
+            if (!isConvex) {
+                //reject the triangle
+                start++;
+                triangles.erase(triangles.begin() + triangles.size() - 1);
+                continue;
+            }
+
+            bool noPointsIn = true;
+
+            for (unsigned int i = 0; i < verts.size(); i++)
+                if (i != start && i != middle && i != end)
+                    if (isPointInTriangle(verts[i].getPosition(), verts[start].getPosition(), verts[middle].getPosition(), verts[end].getPosition()) && noPointsIn) {
+                        //there's a point in triangle
+                        noPointsIn = false;
+                        //reject the triangle
+                        start++;
+                        triangles.erase(triangles.begin() + triangles.size() - 1);
+                        break;
+                    }
+
+            if (noPointsIn) {
+                earNotFound = false;
+                //add triangle to set
+                verts.erase(verts.begin() + middle);
+            }
+        }
+    }
+}
+
 
 int main()
 {
     bool canInteract = true;
     bool delayEnabled = true;
+    bool coolEnabled = true;
+    cout << "cool and delay are enabled" << endl;
 
-	window.create(sf::VideoMode(800, 600), "triangulation (r - reset, d - delay on/off)", sf::Style::Resize | sf::Style::Close);
+	window.create(sf::VideoMode(800, 600), "triangulation (r - reset, d - delay on/off, c - cool on/off)", sf::Style::Resize | sf::Style::Close);
 
 	while (window.isOpen())
 	{
@@ -175,14 +250,22 @@ int main()
                 cout << "delay " << (delayEnabled ? "en" : "dis") << "abled" << endl;
             }
 
+            //enabling/disabling coolness
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::C) {
+                coolEnabled = !coolEnabled;
+                cout << "coolness " << (delayEnabled ? "en" : "dis") << "abled" << endl;
+            }
+
             //reset
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R) {
+                cout << "cool and delay are enabled" << endl;
+                coolEnabled = true;
+                delayEnabled = true;
 				canInteract = true;
 				cout.clear();
 				points.clear();
 				lines.clear();
 				triangles.clear();
-                system("cls");
 			}
 
 
@@ -216,7 +299,10 @@ int main()
                     }
                 }
 				if (!intersectionPresent){
-					closeShape(delayEnabled);
+                    //complete the shape (there's no intersecting lines)
+                    lines.push_back(line(points[0], points[points.size() - 1]));
+					//closeShape(points, delayEnabled, coolEnabled);
+                    triangulation(points);
 					canInteract = false;
 				}
 			}
--- Ear Clipping Triangulation/Ear Clipping Triangulation/main.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-double angleBetweenPoints(point p1, point p2, point p3);
