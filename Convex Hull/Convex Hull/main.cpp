8e17d487241c454a51457e844978488d8e6620aa
Wawrzyn321 <Wawrzyn321@gmail.com>
2016-10-28 20:39:16 +0200

    Algorithm complete (Convex Hull)

--- Convex Hull/Convex Hull/main.cpp
+++ Convex Hull/Convex Hull/main.cpp
@@ -19,6 +19,8 @@ vector<point> points;
 vector<line> lines;
 line currentLine;
 
+typedef sf::Vector3f LineEquation;
+
 void refreshWindow() {
     window.clear(sf::Color::Black);
     for (unsigned i = 0; i < points.size(); i++) {
@@ -39,35 +41,90 @@ double angleBetweenPoints(point p1, point p2) {
 
 void addRandomPoints(unsigned pointsNum, vector<point> &points, sf::Vector2u windowSize) {
     unsigned shiftX = windowSize.x / 10;
-    unsigned shiftY = windowSize.y/ 10;
+    unsigned shiftY = windowSize.y / 10;
     for (unsigned i = 0; i < pointsNum; i++) {
-        float x = (float)(shiftX+rand() % (windowSize.x*4/5));
-        float y = (float)(shiftY+rand() % (windowSize.y*4/5));
+        float x = (float)(shiftX + rand() % (windowSize.x * 4 / 5));
+        float y = (float)(shiftY + rand() % (windowSize.y * 4 / 5));
         points.push_back(point(x, y));
     }
 }
 
-int getRightMostIndex(vector<point> &points) {
-    int pointIndex = 0;
-    for (unsigned i = 1; i < points.size(); i++) {
-        if (points[pointIndex].x > points[i].x) {
-            pointIndex = i;
-        }
-    }
-    return pointIndex;
+double getSide(sf::Vector2f v, sf::Vector2f v1, sf::Vector2f v2) {
+    return (v1.x - v2.x) *(v.y - v2.y) - (v1.y - v2.y)*(v.x - v2.x);
 }
 
-int getLeftMostIndex(vector<point> &points) {
-    int pointIndex = 0;
-    for (unsigned i = 1; i < points.size(); i++) {
-        if (points[pointIndex].x  < points[i].x) {
-            pointIndex = i;
+point *getLeftMost(vector<point> &v) {
+    point *p = NULL;
+    float mX = windowSize;
+    for (unsigned i = 0; i < v.size(); i++) {
+        if (v[i].x < mX) {
+            mX = v[i].x;
+            p = &v[i];
         }
     }
-    return pointIndex;
+    return p;
 }
 
+point *getRightMost(vector<point> &v) {
+    point *p = NULL;
+    float mX = 0;
+    for (unsigned i = 0; i < v.size(); i++) {
+        if (v[i].x > mX) {
+            mX = v[i].x;
+            p = &v[i];
+        }
+    }
+    return p;
+}
 
+vector<point*> getPointsOnRightOrTop(vector<point> &v, point *vL, point *vP) {
+    vector<point*> r;
+    for (unsigned i = 0; i < v.size(); i++) {
+        if (getSide(v[i].getPosition(), vL->getPosition(), vP->getPosition()) > 0) {
+            r.push_back(&v[i]);
+        }
+    }
+    return r;
+}
+vector<point*> getPointsOnRightOrTop(vector<point*> v, point *vL, point *vP) {
+    vector<point*> r;
+    for (unsigned i = 0; i < v.size(); i++) {
+        if (getSide(v[i]->getPosition(), vL->getPosition(), vP->getPosition()) > 0) {
+            r.push_back(v[i]);
+        }
+    }
+    return r;
+}
+
+LineEquation getLineEquation(sf::Vector2f a, sf::Vector2f b) {
+    if (a.x == b.x) {
+        return sf::Vector3f(1, 0, -a.x);
+    }
+    else if (a.y == b.y) {
+        return sf::Vector3f(0, 1, -a.y);
+    }
+    else {
+        float bV = (a.x - b.x) / (a.y - b.y);
+        float cV = (bV*a.y - a.x);
+        return sf::Vector3f(1, -bV, cV);
+    }
+}
+
+point * getFarthestPoint(LineEquation le, vector<point*> v) {
+    double len = 0;
+    point *r = NULL;
+    for (unsigned i = 0; i < v.size(); i++) {
+        //a co jak a i be to zera?
+        double l =
+            abs(le.x*v[i]->x + le.y*v[i]->y + le.z)
+            / sqrt(le.x*le.x + le.y*le.y);
+        if (l > len) {
+            len = l;
+            r = v[i];
+        }
+    }
+    return r;
+}
 //
 //void makeConvexHull(vector<point> &points, vector<line> &lines, line &currentLine) {
 //    int p1Index = getFirstPointYMinIndex(points, currentLine);
@@ -105,6 +162,50 @@ int getLeftMostIndex(vector<point> &points) {
 //
 //}
 
+vector<point*> QuickHull(point A, point B, vector<point*> points)
+{
+    point *C = getFarthestPoint(getLineEquation(A.getPosition(), B.getPosition()), points);
+
+    vector<point*> s1, s2, arg1, arg3;
+
+    if (C) {
+        s1 = getPointsOnRightOrTop(points, &A, C);
+        s2 = getPointsOnRightOrTop(points, C, &B);
+
+        arg1 = QuickHull(A, *C, s1);
+        arg3 = QuickHull(*C, B, s2);
+    }
+
+    vector<point*> vec;
+    vec.insert(vec.end(), arg1.begin(), arg1.end());
+    if (C) {
+        vec.push_back(C);
+    }
+    vec.insert(vec.end(), arg3.begin(), arg3.end());
+    return vec;
+}
+
+vector<point*> otoczka(vector<point> points) {
+    point *A = getLeftMost(points);
+    point *B = getRightMost(points);
+
+    vector<point*> s1 = getPointsOnRightOrTop(points, A, B);
+    vector<point*> s2 = getPointsOnRightOrTop(points, B, A);
+
+    vector<point*> arg2 = QuickHull(*A, *B, s1);
+    vector<point*> arg4 = QuickHull(*B, *A, s2);
+
+    vector<point*> vec;
+    if (A) {
+        vec.push_back(A);
+    }
+    vec.insert(vec.end(), arg2.begin(), arg2.end());
+    if (B) {
+        vec.push_back(B);
+    }
+    vec.insert(vec.end(), arg4.begin(), arg4.end());
+    return vec;
+}
 
 int main() {
     srand((unsigned int)time(NULL));
@@ -114,17 +215,26 @@ int main() {
     sf::Clock timer;
     timer.restart();
 
-    addRandomPoints(100, points, window.getSize());
-    vector<point> p = otoczka(points);
-
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-        }
 
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R) {
+                points.clear();
+                lines.clear();
+                addRandomPoints(100, points, window.getSize());
+                vector<point*> p = otoczka(points);
+                for (unsigned i = 0; i < p.size(); i++) {
+                    point p1 = *p[i];
+                    point p2 = *p[(i + 1) % p.size()];
+                    lines.push_back(line(p1, p2));
+                }
+            }
+
+        }
 
 
         //handling time
